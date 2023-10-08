6c61a9b6547c1aa8283799675269f9a4437a947a
yosephwinata <yoseph.winata@gmail.com>
2023-10-08 19:55:59 +0700

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      'soft-blue': '#8bacda',
+      'cyan': '#00fff7',
+      'very-dark-blue-main-bg': '	#0d192b',
+      'very-dark-blue-card-bg': '#14253d',
+      'very-dark-blue-line': '#2f415b',
+      'white': '#fff'
+    },
+    extend: {},
+  },
+  plugins: [],
+};
